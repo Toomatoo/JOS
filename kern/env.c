387e9b29e9df12fc511db44e736662fa3eb364fd
Jack Liu <mail2jackliu@gmail.com>
2014-03-26 20:56:52 +0800

    Ex.2 Lab3

--- kern/env.c
+++ kern/env.c
@@ -510,7 +510,7 @@ env_run(struct Env *e)
 	//	e->env_tf to sensible values.
 
 	// LAB 3: Your code here.
-	if(!curenv && curenv->env_status == ENV_RUNNING) {
+	if(curenv && curenv->env_status == ENV_RUNNING) {
 		curenv->env_status = ENV_RUNNABLE;
 	}
 	curenv = e;
